9e1b2b5ddeda1a509b9b51df8ec68bac6cfc65a5
Patryk Rosenkiewicz <patryk.rosenkiewicz@tsh.io>
2026-07-02 12:07:50 +0200

    fix: moved cart validate to portal repo

--- src/workflows/hooks/complete-cart-validate.ts
+++ src/workflows/hooks/complete-cart-validate.ts
@@ -1,12 +1,12 @@
 import type { CartWorkflowDTO } from "@medusajs/framework/types"
-import { completeCartWorkflow } from "@medusajs/medusa/core-flows"
 import { MedusaError } from "@medusajs/framework/utils"
 import { canCompleteCart } from "../../common/utils/can-complete-cart"
 
-completeCartWorkflow.hooks.validate((data) => {
-  const cart: CartWorkflowDTO = data.cart
-
+export const validateNoSubscriptionItemsInCart = (cart: CartWorkflowDTO): void => {
   if (!canCompleteCart(cart)) {
-    throw new MedusaError(MedusaError.Types.INVALID_DATA, "Unable to directly complete cart with subscription items")
+    throw new MedusaError(
+      MedusaError.Types.INVALID_DATA,
+      "Unable to directly complete cart with subscription items",
+    )
   }
-})
+}
